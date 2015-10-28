605233242af413dbb3f07cc5b9251790db51e284
AaronHwang <aaronhwang@foxmail.com>
2015-10-28 09:55:47 +0800

    update

--- 1.2.cpp
+++ 1.2.cpp
@@ -2,19 +2,22 @@
 #include <cstring>
 using namespace std;
 
-void swap(char &a, char &b){
+void swap(char &a, char &b)
+{
     a = a^b;
     b = a^b;
     a = a^b;
 }
 
-void reverse2(char *s){
+void reverse2(char *s)
+{
     int n = strlen(s);
     for(int i=0; i<n/2; ++i)
         swap(s[i], s[n-i-1]);
 }
 
-void reverse1(char *s){
+void reverse1(char *s)
+{
     if(!s) return;
     char *p = s, *q = s;
     while(*q) ++q;
@@ -23,9 +26,13 @@ void reverse1(char *s){
         swap(*p++, *q--);
 }
 
-int main(){
+int main()
+{
     char s[] = "1234567890";
+    cout << strlen(s) << endl;
     reverse1(s);
+    cout << s << endl;
+    reverse2(s);
     cout<<s<<endl;
     return 0;
 }
